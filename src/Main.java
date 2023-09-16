8cdf30614409ee5c9259fcd67d5baaf6f6171473
Andrey.Skripay <lordktan@gmail.com>
2023-09-16 22:17:18 +0500

    Задача 1 Назвал переменные согласно правилам Java.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        int a = 10000;
-        System.out.println("Значение переменной a с типом int равно" + a );
-        byte b = 11;
-        System.out.println("Значение переменной a с типом byte равно" + b );
-        short c = -10000;
-        System.out.println("Значение переменной a с типом short равно" + c );
-        long d = 1000000L;
-        System.out.println("Значение переменной a с типом long равно" + d );
-        float e = 1.11f;
-        System.out.println("Значение переменной a с типом float равно" + e );
-        double i = 11.11;
-        System.out.println("Значение переменной a с типом double равно" + i );
+        int aInt = 10000;
+        System.out.println("Значение переменной a с типом int равно " + aInt );
+        byte bByte = 11;
+        System.out.println("Значение переменной a с типом byte равно " + bByte );
+        short cShort = -10000;
+        System.out.println("Значение переменной a с типом short равно " + cShort );
+        long dLong = 1000000L;
+        System.out.println("Значение переменной a с типом long равно " + dLong );
+        float eFloat = 1.11f;
+        System.out.println("Значение переменной a с типом float равно " + eFloat );
+        double iDouble = 11.11;
+        System.out.println("Значение переменной a с типом double равно " + iDouble );
+
+
+
 
     }
 }
