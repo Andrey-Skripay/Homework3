ec12b9f6f814d0c12cf6612158f7618bea91aa22
Andrey.Skripay <lordktan@gmail.com>
2023-09-16 22:08:25 +0500

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 10000;
+        System.out.println("Значение переменной a с типом int равно" + a );
+        byte b = 11;
+        System.out.println("Значение переменной a с типом byte равно" + b );
+        short c = -10000;
+        System.out.println("Значение переменной a с типом short равно" + c );
+        long d = 1000000L;
+        System.out.println("Значение переменной a с типом long равно" + d );
+        float e = 1.11f;
+        System.out.println("Значение переменной a с типом float равно" + e );
+        double i = 11.11;
+        System.out.println("Значение переменной a с типом double равно" + i );
+
+    }
+}
